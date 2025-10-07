36e5a2f1a278da4103fc25047d3569e37d7fd8a2
Mohamed Khairallah <75247823+khairallah17@users.noreply.github.com>
2025-10-07 15:49:41 +0100

    Fix custom user model manager and admin forms

--- backend/primini_backend/users/models.py
+++ backend/primini_backend/users/models.py
@@ -1,5 +1,37 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Custom manager that authenticates users with their email address."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('L\'adresse e-mail doit être renseignée.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Les super utilisateurs doivent avoir is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Les super utilisateurs doivent avoir is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -8,6 +40,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
