7424b20193c1ebee1dac6366e54d8e9f224c00a9
khairallah <med.khairallah3@gmail.com>
2026-02-09 07:37:56 +0100

    fixing db parsing

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"XsM28pltt+vXgIr9sXrMEsoLSbG65/BchRZJ+wbXUpk=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"PhGvet6kXOU/aLPBXS6ymcjz3wY6/sZauSvqd79Cud4=\"}"
